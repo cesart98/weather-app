199f2d8deefec83b95761224e0d633a325f0aecc
Cesar Torres <torres-cesar@outlook.com>
2022-03-06 17:26:23 -0600

    Add geocoding API to retrieve coordinates

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -1,14 +1,20 @@
-async function loadJson() {
+async function getLocation(cityName, stateCode, countryCode) {
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${appid}`;
+
+    let response = await fetch (url, { mode: 'cors' });
+    return response.json();
+}
+
+async function getWeather(lat, lon) {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`;
 
     let response = await fetch (url, { mode: 'cors' });
     return response.json();
 }
 
-let lat = 32.7767;
-let lon = -96.7970;
 let appid = 'dba1cfa92fd8532ebe1d3664c395d25f';
-const city = new Object;
-
-loadJson()
-    .then(json => Object.assign(city, json))
+let city = new Object;
+getLocation('Dallas', 'TX', 'US')
+    .then(props => Object.assign(city, props[0]))
+    .then(city => getWeather(city.lat, city.lon))
+    .then(data => Object.assign(city, data))
