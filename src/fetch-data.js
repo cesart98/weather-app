3bdc829aa5f4a7b61a225d5b1c351f88ed00f1b2
Cesar Torres <torres-cesar@outlook.com>
2022-03-06 17:54:39 -0600

    Rename return variables

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -2,19 +2,21 @@ async function getLocation(cityName, stateCode, countryCode) {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${appid}`;
 
     let response = await fetch (url, { mode: 'cors' });
-    return response.json();
+    let cityData = await response.json()
+    return cityData[0];
 }
 
 async function getWeather(lat, lon) {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`;
 
     let response = await fetch (url, { mode: 'cors' });
-    return response.json();
+    let weatherData = await response.json()
+    return weatherData;
 }
 
 let appid = 'dba1cfa92fd8532ebe1d3664c395d25f';
 let city = new Object;
 getLocation('Dallas', 'TX', 'US')
-    .then(props => Object.assign(city, props[0]))
+    .then(cityData => Object.assign(city, cityData))
     .then(city => getWeather(city.lat, city.lon))
-    .then(data => Object.assign(city, data))
+    .then(weatherData => Object.assign(city, weatherData))
