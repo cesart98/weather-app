cae41d8992c667b4c1740014b3d9a105b9e6c286
Cesar Torres <torres-cesar@outlook.com>
2022-03-07 15:48:39 -0600

    Set form input to weather request

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -1,22 +1,38 @@
-async function getLocation(cityName, stateCode, countryCode) {
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${appid}`;
+function fetchData(event) {
+    function setVariables() {
+        cityName = event.target[0].value;
+        stateCode = event.target[1].value;
+    }
+    async function getLocation(cityName, stateCode, countryCode) {
+        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${appid}`;
+    
+        let response = await fetch (url, { mode: 'cors' });
+        let cityData = await response.json()
+        return cityData[0];
+    } 
+    async function getWeather(lat, lon) {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`;
+    
+        let response = await fetch (url, { mode: 'cors' });
+        let weatherData = await response.json()
+        return weatherData;
+    }
 
-    let response = await fetch (url, { mode: 'cors' });
-    let cityData = await response.json()
-    return cityData[0];
+    let cityName;
+    let stateCode;
+    let appid = 'dba1cfa92fd8532ebe1d3664c395d25f';
+    
+    setVariables();
+    getLocation(cityName, stateCode, 'US')
+        .then(cityLocation => Object.assign(city, cityLocation))
+        .then(city => getWeather(city.lat, city.lon))
+        .then(cityWeather => Object.assign(city, cityWeather));
+    event.preventDefault();
+
+    return city;
 }
 
-async function getWeather(lat, lon) {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`;
-
-    let response = await fetch (url, { mode: 'cors' });
-    let weatherData = await response.json()
-    return weatherData;
-}
-
-let appid = 'dba1cfa92fd8532ebe1d3664c395d25f';
 let city = new Object;
-getLocation('Dallas', 'TX', 'US')
-    .then(cityData => Object.assign(city, cityData))
-    .then(city => getWeather(city.lat, city.lon))
-    .then(weatherData => Object.assign(city, weatherData))
+
+let form = document.querySelector('form');
+form.addEventListener('submit', event => fetchData(event));
