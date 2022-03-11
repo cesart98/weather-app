0973ec3010577c18abea99940f92c1d090cceaed
Cesar Torres <torres-cesar@outlook.com>
2022-03-11 15:13:57 -0600

    Add inline source map for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    clean: true,
   },
+  devtool: 'inline-source-map',
 };
