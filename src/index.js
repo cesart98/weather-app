07336e5315677b0cd4d0675ddfaa2fa291eb5970
Cesar Torres <torres-cesar@outlook.com>
2022-03-11 18:46:49 -0600

    Populate weather data to webpage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import {fetchData} from './fetch-data.js'
 import {populateWebpage} from './populate-webpage.js'
 
+function main(event) {
+    fetchData(event)
+        .then(data => populateWebpage(data));
+}
+
 let form = document.querySelector('form');
-form.addEventListener('submit', event => fetchData(event)
-    .then(data => populateWebpage(data))
-    )
+form.addEventListener('submit', main)
--- src/populate-webpage.js
+++ src/populate-webpage.js
@@ -1,3 +1,34 @@
 export function populateWebpage(data) {
-    console.log(data);
+    function populateCityName() {
+        let cityName = document.querySelector('.city-name');
+        cityName.textContent = city.name;
+    }
+    function populateTemps() {
+        function populateCurrentTemp() {
+            let currentTemp = document.querySelector('.current-temp');
+            currentTemp.textContent = city.main.feels_like;
+        }
+        function populateHighTemp() {
+            let highTemp = document.querySelector('.high-temp');
+            highTemp.textContent = city.main.temp_max;
+        }
+        function populateLowTemp() {
+            let lowTemp = document.querySelector('.low-temp');
+            lowTemp.textContent = city.main.temp_min;
+        }
+        populateCurrentTemp();
+        populateHighTemp();
+        populateLowTemp();
+    }
+    function populateWeatherDescription() {
+        let weatherDescription = document.querySelector('.weather-description');
+        weatherDescription.textContent = city.weather[0].description;
+    }
+
+    let city = Object.create(data);
+
+    populateCityName();
+    populateTemps();
+    populateWeatherDescription();
+    console.log(city);
 }
